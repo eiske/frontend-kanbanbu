d4d511435fd4facba747463bf8945c3ab2228dc5
Eiske <hashimoto.eisk@gmail.com>
2023-10-25 23:24:19 -0300

    configuring api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://studynizer-backend-production.up.railway.app",
+  // baseURL: "https://backend-kanbanbu-production.up.railway.app",
+  baseURL: "http://localhost:5000",
 });
 
 export default api;
