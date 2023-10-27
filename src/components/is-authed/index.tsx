807ce4cda2392250690b5e1ced654d54f2c329bd
Eiske <hashimoto.eisk@gmail.com>
2023-10-27 19:40:35 -0300

    Created authed component

--- src/components/is-authed/index.tsx
+++ src/components/is-authed/index.tsx
@@ -1,26 +1,23 @@
-/* eslint-disable consistent-return */
-import { ACCESS_TOKEN_KEY } from 'constants/storage-keys';
-import { useRouter } from 'next/router';
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { USER_TOKEN_KEY } from "@/constants/storage-keys";
+import { useRouter } from "next/router";
+import { PropsWithChildren, useEffect, useState } from "react";
 
 const IsAuthed = ({ children }: PropsWithChildren<{}>) => {
     const router = useRouter();
     const [isLoggedIn, setIsloggedIn] = useState(false);
-    let redirectUrl = '/login';
+    let redirectUrl = "/login";
 
     useEffect(() => {
         if (isLoggedIn && router.isReady) router.push(router.asPath);
     }, [isLoggedIn, router.isReady]);
 
     useEffect(() => {
-        setIsloggedIn(!!localStorage.getItem('accessToken'));
-        
-        if (!localStorage.getItem('accessToken')) router.push(redirectUrl);
+        setIsloggedIn(!!localStorage.getItem(USER_TOKEN_KEY));
+
+        if (!localStorage.getItem(USER_TOKEN_KEY)) router.push(redirectUrl);
     }, []);
 
-    return (
-        <div>{isLoggedIn && children}</div>
-    );
+    return <div>{isLoggedIn && children}</div>;
 };
 
 export default IsAuthed;
