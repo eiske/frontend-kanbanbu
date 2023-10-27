46a5dcef86b2445511a1e921a91b5d406399700e
Eiske <hashimoto.eisk@gmail.com>
2023-10-27 19:44:20 -0300

    Updated api base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://backend-kanbanbu-production.up.railway.app",
-  // baseURL: "http://localhost:5000",
   headers: {
     'Content-Type': 'application/json',
     Authorization: 'Bearer',
