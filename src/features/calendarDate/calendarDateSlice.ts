669f09dda08a6089e23493c9904aae68913bb73b
Eiske <hashimoto.eisk@gmail.com>
2023-10-27 19:42:14 -0300

    Created calendarDate reducer

---
+++ src/features/calendarDate/calendarDateSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface CalendarDateState {
+  date: Date[];
+}
+
+const initialState: CalendarDateState = {
+  date: [],
+};
+
+const calendarDateSlice = createSlice({
+  name: 'calendarDate',
+  initialState,
+  reducers: {
+    setCalendarDate: (state, {payload}: PayloadAction<Date>) => {
+      state.date = [...state.date, payload];
+    },
+  },
+});
+
+export const { setCalendarDate } = calendarDateSlice.actions;
+export default calendarDateSlice.reducer;
