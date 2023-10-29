5bfc38a3acbcdb9a613064dfa1b5128e5a2427f1
Eiske <hashimoto.eisk@gmail.com>
2023-10-29 19:35:28 -0300

    Updated user state and using store interface

--- src/hooks/use-user/index.tsx
+++ src/hooks/use-user/index.tsx
@@ -9,10 +9,13 @@ import { setUserInfo } from "@features/userSession/userSessionSlice";
 import { useEffect, useState } from "react";
 import { notification } from "antd";
 import { useRouter } from "next/router";
+import { UserInfo } from "@services/user/types";
+import { getUserId, getUserName, getUserToken } from "@services/utils";
 
 const useUser = () => {
     const dispatch = useDispatch();
     const router = useRouter();
+    const [user, setUser] = useState<UserInfo>();
     const [isLogin, setIsLogin] = useState(false);
     const [isSignUp, setIsSignUp] = useState(false);
     const [signed, setSigned] = useState(false);
@@ -21,19 +24,21 @@ const useUser = () => {
         setSigned(!!localStorage.getItem(USER_TOKEN_KEY));
     }, [localStorage.getItem(USER_TOKEN_KEY)]);
 
+    useEffect(() => {
+        setUser({
+            name: getUserName(),
+            id: getUserId(),
+            token: getUserToken(),
+        });
+    }, []);
+
     const userLogin = async (email: string, password: string) => {
         try {
             setIsLogin(true);
             const response = await login(email, password);
-            localStorage.setItem(
-                USER_TOKEN_KEY,
-                JSON.stringify(response.data.token)
-            );
-            localStorage.setItem(USER_ID_KEY, JSON.stringify(response.data.id));
-            localStorage.setItem(
-                USER_NAME_KEY,
-                JSON.stringify(response.data.name)
-            );
+            localStorage.setItem(USER_TOKEN_KEY, response.data.token);
+            localStorage.setItem(USER_ID_KEY, response.data.id);
+            localStorage.setItem(USER_NAME_KEY, response.data.name);
             dispatch(setUserInfo(response?.data));
         } catch (error: any) {
             notification.info({
@@ -79,6 +84,7 @@ const useUser = () => {
         signUp: userSignUp,
         signed,
         fetching: isLogin || isSignUp,
+        user,
     };
 };
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,21 @@
 import axios from "axios";
+import { getUserToken } from "./utils";
 
 const api = axios.create({
-  baseURL: "https://backend-kanbanbu-production.up.railway.app",
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: 'Bearer',
-  },
+    baseURL: "https://backend-kanbanbu-production.up.railway.app",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer",
+    },
+});
+
+api.interceptors.request.use(async (config) => {
+    const tokenResponse = getUserToken();
+
+    if (config.headers && tokenResponse) {
+        config.headers.Authorization = `Bearer ${tokenResponse}`;
+    }
+    return config;
 });
 
 export default api;
