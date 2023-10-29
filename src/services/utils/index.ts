8fe1329a6bdd3fb556af352fd487eae5cbac975e
Eiske <hashimoto.eisk@gmail.com>
2023-10-29 19:33:59 -0300

    Created interface to get storage items

---
+++ src/services/utils/index.ts
@@ -0,0 +1,12 @@
+import {
+    USER_ID_KEY,
+    USER_NAME_KEY,
+    USER_TOKEN_KEY,
+} from "@constants/storage-keys";
+
+export const getUserId = () => localStorage.getItem(USER_ID_KEY) as string;
+
+export const getUserToken = () =>
+    localStorage.getItem(USER_TOKEN_KEY) as string;
+
+export const getUserName = () => localStorage.getItem(USER_NAME_KEY) as string;
