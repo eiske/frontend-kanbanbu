40bfbfa4966d4578d2715ae2c8e4f7de5eefc7bf
Eiske <hashimoto.eisk@gmail.com>
2023-10-29 13:12:48 -0300

    Created test for login form

---
+++ src/pages/login/components/login-form/index.test.tsx
@@ -0,0 +1,49 @@
+import "@testing-library/jest-dom";
+import "../../../../../.jest/matchMedia";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import LoginForm from ".";
+import { Provider } from "react-redux";
+import store from "@/store";
+
+const mockLogin = jest.fn();
+const mockPush = jest.fn();
+
+jest.mock("next/router", () => ({
+    useRouter: () => ({
+        push: (str: string) => mockPush(str),
+    }),
+}));
+
+jest.mock("@hooks/use-user", () => ({
+    __esModule: true,
+    default: () => ({
+        fetching: false,
+        login: mockLogin,
+        signed: true,
+    }),
+}));
+
+describe("<LoginForm />", () => {
+    it("should call login function", async () => {
+        render(
+            <Provider store={store}>
+                <LoginForm />
+            </Provider>
+        );
+        const emailInput = screen.getByPlaceholderText("Email");
+        const passwordInput = screen.getByPlaceholderText("Senha");
+        const loginButton = screen.getByText("Entrar");
+
+        fireEvent.change(emailInput, {
+            target: { value: "test@example.com" },
+        });
+        fireEvent.change(passwordInput, {
+            target: { value: "password123" },
+        });
+        fireEvent.click(loginButton);
+
+        await waitFor(() => {
+            expect(mockLogin).toHaveBeenCalled();
+        });
+    });
+});
