8992676f7d21a26f3334dc6070e82def44fc58cd
Eiske <hashimoto.eisk@gmail.com>
2023-10-30 22:42:29 -0300

    Hello from board and testing custom board hook

--- src/components/board/index.tsx
+++ src/components/board/index.tsx
@@ -1,3 +1,9 @@
-const Board = () => <h1>Hello from Board</h1>;
+import useBoard from "@hooks/use-board";
+
+const Board = () => {
+    const { board } = useBoard();
+    console.log("board: ", board);
+    return <h1>Hello from Board</h1>;
+};
 
 export default Board;
