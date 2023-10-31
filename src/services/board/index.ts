07471cf68ed68a8a5c5d2a1e268f96b8d9266c85
Eiske <hashimoto.eisk@gmail.com>
2023-10-30 22:41:37 -0300

    Created get task fetcher

---
+++ src/services/board/index.ts
@@ -0,0 +1,22 @@
+import { AxiosResponse } from "axios";
+import api from "../api";
+import { getUserId } from "../utils";
+import type { BoardColumn, Task } from "./index.types";
+
+export const getTasks = async () => {
+    const [resTasksTodo, resTasksDoing, resTasksCompleted] = await Promise.all<
+        AxiosResponse<Task[]>
+    >([
+        api.get(`/user/board-tasks-todo/${getUserId()}`),
+        api.get(`/user/board-tasks-doing/${getUserId()}`),
+        api.get(`/user/board-tasks-completed/${getUserId()}`),
+    ]);
+
+    return {
+        tasksTodo: resTasksTodo.data,
+        tasksDoing: resTasksDoing.data,
+        tasksCompleted: resTasksCompleted.data,
+    };
+};
+
+export type { BoardColumn, Task };
