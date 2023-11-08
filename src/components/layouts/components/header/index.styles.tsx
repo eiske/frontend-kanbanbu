2b5e2c37c1f7921d03c315e9b5636d681eda387c
Eiske <hashimoto.eisk@gmail.com>
2023-11-08 18:26:26 -0300

    Created header component

---
+++ src/components/layouts/components/header/index.styles.tsx
@@ -0,0 +1,105 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .logo {
+        width: 40%;
+        cursor: default;
+
+        h1 {
+            border-radius: 4px;
+            margin: 0 0 30px 30px;
+            font-weight: bold;
+            background: black;
+            color: white;
+            text-align: center;
+            padding: 0 50px;
+        }
+    }
+
+    @media (max-width: 990px) {
+        flex-direction: column;
+        text-align: center;
+
+        .logo {
+            width: 100%;
+
+            h1 {
+                margin: 0 0 30px 0;
+                border-radius: 0;
+            }
+        }
+    }
+`;
+
+export const Menu = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 30px;
+    width: 100%;
+
+    a {
+        color: #000;
+        text-decoration: none;
+        font-weight: bold;
+        margin: 10px 40px;
+        border: 1px solid lightgray;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+        border-bottom-left-radius: 0px;
+        border-bottom-right-radius: 0px;
+        padding: 8px 20px;
+    }
+
+    a.active {
+        background: #000;
+        color: #fff;
+    }
+
+    a:hover {
+        background: #000;
+        color: #fff;
+        text-decoration:none;
+        cursor:pointer;
+        transition: 0.3s;
+    }
+
+    @media (max-width: 990px) {
+        flex-direction: column;
+        text-align: center;
+    }
+`;
+
+export const LogOut = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 30px;
+    margin-left: 60px;
+
+    a {
+        color: #000;
+        text-decoration: none;
+        font-weight: bold;
+        margin-left: 10px;
+        margin-right: 66px;
+        border: 1px solid lightgray;
+        border-radius: 10px 0;
+        padding: 8px 20px;
+    }
+
+    a:hover {
+        background: #000;
+        color: #fff;
+        text-decoration:none;
+        cursor:pointer;
+        transition: 0.3s;
+    }
+
+    @media (max-width: 990px) {
+        flex-direction: column;
+        text-align: center;
+    }
+`;
---
+++ src/components/layouts/components/header/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Link from 'next/link';
+import useUser from '@hooks/use-user';
+import { Button } from 'antd';
+import { useRouter } from 'next/router';
+import { Container, Menu, LogOut } from './index.styles';
+
+const Header = () => {
+    const { logout } = useUser();
+    const router = useRouter();
+    return (
+        <Container>
+            <div className="logo"><h1>Kanbanbu</h1></div>
+            <Menu>
+                <Link href="/" className={router.pathname === '/' ? 'active' : ''}>Board</Link>
+                <Link href="/subjects-list" className={router.pathname === '/subjects-list' ? 'active' : ''}>Disciplinas</Link>
+                <Link href="/agenda" className={router.pathname === '/agenda' ? 'active' : ''}>Agenda</Link>
+            </Menu>
+            <LogOut>
+                <Button href="#" onClick={logout}>Sair</Button>
+            </LogOut>
+        </Container>
+    );
+};
+
+export default Header;
