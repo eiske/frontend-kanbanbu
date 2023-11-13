f61b5cd9ce73bf20b2040e8c2b3fab39c3914252
Eiske <hashimoto.eisk@gmail.com>
2023-11-12 23:26:29 -0300

    Created markdown service

---
+++ src/services/markdown/index.ts
@@ -0,0 +1,54 @@
+import api from '@services/api';
+import { getUserId } from '@services/utils';
+import latinize from 'latinize';
+
+type SaveType = {
+    annotationBlockValue: string,
+    savePageName: string,
+    saveUrlId: string,
+    savePageId: string,
+    subjectName: string,
+    saveMarkdownId: string
+}
+
+type CreateNewPage = {
+    initialEditorState: any,
+    newPageName: string,
+    subjectPageLink: string,
+    pageId: string,
+    subjectName: string
+}
+
+export const saveMarkdown = ({
+    annotationBlockValue,
+    savePageName,
+    saveUrlId,
+    savePageId,
+    subjectName,
+    saveMarkdownId,
+}: SaveType) => api.put(`/user/markdown/${saveMarkdownId}`, {
+    annotation_block: { annotationBlock: annotationBlockValue },
+    page_name: savePageName,
+    url_id: saveUrlId,
+    page_id: savePageId,
+    subject_name: subjectName,
+});
+
+export const getMarkdown = () => api.get(`/user/markdown/${getUserId()}`);
+
+export const createNewPage = ({
+    initialEditorState,
+    newPageName,
+    pageId,
+    subjectName,
+    subjectPageLink,
+}: CreateNewPage) => api.post('/user/markdown', {
+    annotation_block: { annotationBlock: initialEditorState },
+    page_name: latinize(newPageName),
+    url_id: subjectPageLink,
+    page_id: pageId,
+    subject_name: subjectName,
+    users_id: getUserId(),
+});
+
+export const deletePage = (markdownId: string) => api.delete(`/user/markdown/${markdownId}`);
