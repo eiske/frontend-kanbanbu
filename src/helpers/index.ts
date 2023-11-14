e09769d23208f3a74cc13728a7de7678f0df2b39
Eiske <hashimoto.eisk@gmail.com>
2023-11-13 21:29:38 -0300

    Created calendar helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,5 @@
 import { Task } from '@services/board';
+import { v4 } from 'uuid';
 
 interface InitialBoard {
     tasksTodo: Task[];
@@ -23,3 +24,37 @@ export const createInitialBoard = ({
     doingList: mapResponseToTasks(tasksDoing),
     completedList: mapResponseToTasks(tasksCompleted),
 });
+
+const renderPriorityColor = (priority: string) => {
+    if (priority === 'Alta') {
+        return '#E77669';
+    } if (priority === 'Baixa') {
+        return '#BEEC5A';
+    }
+
+    return '#EEE950';
+};
+
+export const generateDemoEvents = (taskData: any) => {
+    const events = [];
+
+    const taskEvents = taskData.map((task: any) => {
+        const taskEvent = {
+            id: v4(),
+            startAt: task.due_date_start,
+            endAt: task.due_date_end,
+            summary: task.title,
+            color: renderPriorityColor(task.priority),
+            allDay: false,
+
+        };
+
+        return taskEvent;
+    });
+
+    for (let i = 0; i < taskEvents.length; i++) {
+        events.push(taskEvents[i]);
+    }
+
+    return events;
+};
