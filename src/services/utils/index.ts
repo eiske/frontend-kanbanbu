72107a1fd3a891037f95b03ea46169a6bef5baa2
Eiske <hashimoto.eisk@gmail.com>
2023-11-15 23:25:40 -0300

    Linting

--- src/services/utils/index.ts
+++ src/services/utils/index.ts
@@ -2,11 +2,10 @@ import {
     USER_ID_KEY,
     USER_NAME_KEY,
     USER_TOKEN_KEY,
-} from "@constants/storage-keys";
+} from '@constants/storage-keys';
 
 export const getUserId = () => localStorage.getItem(USER_ID_KEY) as string;
 
-export const getUserToken = () =>
-    localStorage.getItem(USER_TOKEN_KEY) as string;
+export const getUserToken = () => localStorage.getItem(USER_TOKEN_KEY) as string;
 
 export const getUserName = () => localStorage.getItem(USER_NAME_KEY) as string;
