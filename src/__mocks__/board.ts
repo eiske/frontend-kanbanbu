5f7beed975195d4db96466ef74640a98db7a5e72
Eiske <hashimoto.eisk@gmail.com>
2023-11-15 23:24:05 -0300

    Created mock for brad

---
+++ src/__mocks__/board.ts
@@ -0,0 +1,60 @@
+export const mockedBoard = {
+    Tarefas: {
+        title: 'Tarefas',
+        items: [
+            {
+                id: 'ea6595ae-4afc-42ec-a8f1-1d82dd85baf3',
+                name: 'Matematica',
+                description: 'Um mais dois',
+                priority: 'Alta',
+                date: [
+                    '2023-11-11T15:02:03.000Z',
+                    '2023-11-18T12:40:14.000Z',
+                ],
+            },
+        ],
+        columnType: 'todo',
+    },
+    Fazendo: {
+        title: 'Fazendo',
+        items: [
+            {
+                id: 'f9ff9fb0-a441-4112-aa0c-54cc8e6326cb',
+                name: 'TCC',
+                description: 'Trabalho de conclusao de curso',
+                priority: 'Alta',
+                date: [
+                    '2023-09-01T15:08:07.000Z',
+                    '2023-12-04T15:08:28.000Z',
+                ],
+            },
+            {
+                id: '2e1f02bf-d003-4fdf-8197-31ce394c0f0d',
+                name: 'Portugues',
+                description: 'Brasileiro',
+                priority: 'Baixa',
+                date: [
+                    '2023-11-16T15:07:40.000Z',
+                    '2023-11-30T15:07:42.000Z',
+                ],
+            },
+        ],
+        columnType: 'doing',
+    },
+    Concluído: {
+        title: 'Concluído',
+        items: [
+            {
+                id: 'c9b79e9e-5300-48e4-9f29-9ce962cfda82',
+                name: 'TCC',
+                description: 'Documentação',
+                priority: 'Alta',
+                date: [
+                    '2023-01-01T15:08:56.000Z',
+                    '2023-07-31T15:09:02.000Z',
+                ],
+            },
+        ],
+        columnType: 'completed',
+    },
+};
