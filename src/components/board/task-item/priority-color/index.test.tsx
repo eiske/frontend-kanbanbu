1580dffad4031f3eced19b8b6c2d2e88564386ba
Eiske <hashimoto.eisk@gmail.com>
2023-11-15 23:25:14 -0300

    Creted test for priority component

---
+++ src/components/board/task-item/priority-color/index.test.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import Priority from '.';
+
+describe('<Priority />', () => {
+    it('should render a div with the correct background color when a priority level is passed as a prop', () => {
+        const { container } = render(<Priority priorityLevel="Baixa" />);
+        const div = container.firstChild;
+        expect(div).toHaveStyle('background-color: #BEEC5A');
+    });
+});
