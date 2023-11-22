f065f1b0f047bc8049b85c36d2abc909011a1922
vinicius.eiske <vinicius.eiske@valtech.com>
2023-11-22 16:14:57 -0300

    Created test to style-button component

---
+++ src/components/style-button/index.test.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/style-prop-object */
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import StyleButton from '.';
+
+const onToggleMock = jest.fn();
+
+describe('<StyleButton />', () => {
+    beforeEach(() => {
+        render(<StyleButton label="Test Button" onToggle={onToggleMock} style="test-style" />);
+    });
+
+    it('should render a button element with the provided label', () => {
+        const buttonElement = screen.getByText('Test Button');
+        expect(buttonElement).toBeInTheDocument();
+    });
+
+    it('should call the onToggle function with the provided style when the button is clicked', () => {
+        const buttonElement = screen.getByText('Test Button');
+        fireEvent.mouseDown(buttonElement);
+        expect(onToggleMock).toHaveBeenCalledWith('test-style');
+    });
+});
