bf94f5ab94cc5a2eeea07b9b3d9071e4c3621f06
vinicius.eiske <vinicius.eiske@valtech.com>
2023-11-22 16:51:24 -0300

    Fix Priority test

--- src/components/board/task-item/priority-color/index.test.tsx
+++ src/components/board/task-item/priority-color/index.test.tsx
@@ -6,6 +6,6 @@ describe('<Priority />', () => {
     it('should render a div with the correct background color when a priority level is passed as a prop', () => {
         const { container } = render(<Priority priorityLevel="Baixa" />);
         const div = container.firstChild;
-        expect(div).toHaveStyle('background-color: #BEEC5A');
+        expect(div).toHaveStyle('background-color: #EDD975');
     });
 });
