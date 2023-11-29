1dbe71e2faf6c10979c93c3087e46472dade4a5e
Eiske <hashimoto.eisk@gmail.com>
2023-11-29 18:58:56 -0300

    Created drawer

--- src/components/layouts/authed-layout/index.tsx
+++ src/components/layouts/authed-layout/index.tsx
@@ -1,21 +1,38 @@
 import IsAuthed from '@/components/is-authed';
 import store from '@/store';
 import { Layout } from 'antd';
-import { PropsWithChildren, ReactElement } from 'react';
+import { PropsWithChildren, ReactElement, useState } from 'react';
 import { Provider } from 'react-redux';
 import styles from './index.module.css';
 import Header from '../components/header';
+import Drawer from '../components/drawer';
 
 const { Content } = Layout;
 
-const AuthedLayout = ({ children }: PropsWithChildren) => (
-    <Provider store={store}>
-        <Layout className={styles.mainLayout}>
-            <Header />
-            <Content className={styles.mainContent}>{children}</Content>
-        </Layout>
-    </Provider>
-);
+const AuthedLayout = ({ children }: PropsWithChildren) => {
+    const [open, setOpen] = useState(false);
+
+    const onClose = () => {
+        setOpen(false);
+    };
+
+    const onOpen = () => {
+        setOpen(true);
+    };
+
+    return (
+        <Provider store={store}>
+            <Header onOpen={onOpen} />
+            <Layout className={styles.mainLayout}>
+                <Drawer
+                    open={open}
+                    onClose={onClose}
+                />
+                <Content className={styles.mainContent}>{children}</Content>
+            </Layout>
+        </Provider>
+    );
+};
 
 export const AuthedPageLayout = (page: ReactElement) => (
     <IsAuthed>
---
+++ src/components/layouts/components/drawer/index.tsx
@@ -0,0 +1,26 @@
+import { Drawer as AntDrawer } from 'antd';
+import useUser from '@hooks/use-user';
+import MenuArea from '../menu-area';
+import MenuFooter from '../menu-footer';
+
+interface Props {
+    open: boolean;
+    onClose: () => void;
+}
+
+const Drawer = ({ onClose, open }: Props) => {
+    const { logout } = useUser();
+    return (
+        <AntDrawer
+            placement="left"
+            onClose={onClose}
+            open={open}
+            footer={<MenuFooter logout={logout} />}
+            className="custom-drawer-menu"
+        >
+            <MenuArea />
+        </AntDrawer>
+    );
+};
+
+export default Drawer;
