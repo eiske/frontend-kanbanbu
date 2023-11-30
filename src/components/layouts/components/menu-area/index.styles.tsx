35f0c628eedc791e0fa46648cab0f03e9fa966f3
Eiske <hashimoto.eisk@gmail.com>
2023-11-29 22:00:13 -0300

    Update menu area style

--- src/components/layouts/components/menu-area/index.styles.tsx
+++ src/components/layouts/components/menu-area/index.styles.tsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 export const Container = styled.ul`
     display: flex;
     flex-direction: column;
+    list-style-type: none;
+    font-size: 16px;
+    gap: 24px;
+    padding: 10px;
 
     a {
         text-decoration: none;
