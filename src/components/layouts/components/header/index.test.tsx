8c065a729988597bbf23ed963449dce57069cb18
Eiske <hashimoto.eisk@gmail.com>
2023-11-29 22:00:35 -0300

    Created test for header

---
+++ src/components/layouts/components/header/index.test.tsx
@@ -0,0 +1,25 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '@store/index';
+import Header from '.';
+
+jest.mock('next/router', () => ({
+    useRouter: () => ({
+        pathname: '/',
+    }),
+}));
+
+describe('<Header />', () => {
+    it('should render header component', () => {
+        render(
+            <Provider store={store}>
+                <Header onOpen={jest.fn()} />
+            </Provider>
+        );
+        expect(screen.getByAltText('Plataforma de Organização de Estudos')).toBeInTheDocument();
+        expect(screen.getByText('Board')).toBeInTheDocument();
+        expect(screen.getByText('Disciplinas')).toBeInTheDocument();
+        expect(screen.getByText('Agenda')).toBeInTheDocument();
+    });
+});
