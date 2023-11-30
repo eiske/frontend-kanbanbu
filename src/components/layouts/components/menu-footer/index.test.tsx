2b7ded1758f2efeef928bbbbd6b541effa0e890a
Eiske <hashimoto.eisk@gmail.com>
2023-11-29 22:00:48 -0300

    Created test for menu footer

---
+++ src/components/layouts/components/menu-footer/index.test.tsx
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import MenuFooter from '.';
+
+describe('<MenuFooter />', () => {
+    it('should render menu footer', () => {
+        render(
+            <MenuFooter logout={jest.fn()} />
+        );
+        expect(screen.getByText('Sair')).toBeInTheDocument();
+    });
+});
